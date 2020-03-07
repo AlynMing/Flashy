e6aee62b6e4488c57c38466095b2b1fa39bb3b85
luke-awalker <lukewalker@college.harvard.edu>
2020-03-07 00:21:36 -0500

    Added feature - new activity to create new flash card

--- app/src/main/java/com/example/flashy/MainActivity.java
+++ app/src/main/java/com/example/flashy/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.flashy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +21,34 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.answer).setVisibility((View.VISIBLE));
             }
         });
+
+        findViewById(R.id.Add).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ActivityB.class);
+                MainActivity.this.startActivityForResult(intent,111);
+            }
+
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if ((requestCode == 111) && (data != null)) {
+            String Question = data.getExtras().getString("Question");
+            String Answer = data.getExtras().getString("Answer");
+            setContentView(R.layout.activity_main);
+
+            ((TextView) findViewById(R.id.question)).setText(Question);
+            ((TextView)findViewById(R.id.answer)).setText(Answer);
+
+            findViewById(R.id.question).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    findViewById(R.id.question).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.answer).setVisibility((View.VISIBLE));
+                }
+            });
+
+        }
     }
 }
